ac025b6e09c690ef076c58be3722a945dd916ff1
Ellie-kang <kya6873@hanmail.net>
2026-03-19 14:47:33 +0900

    ✨ feat: TodoList 기본 CRUD 및 실시간 시계 기능 구현
    - React 함수형 컴포넌트 및 useState 훅 적용
    - Todo 항목 추가(Create), 조회(Read), 삭제(Delete) 기능 구현
    - 체크박스 상태 토글 기능 추가 (불변성 유지)
    - setInterval을 활용한 실시간 시계(Clock) 컴포넌트 통합
    - 모달(Modal) 상세 보기를 위한 상태 빌드업

--- ReactExam/todolist/src/App.tsx
+++ ReactExam/todolist/src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import './App.css'
+import Todolist from './Todolist'
+import Clock from './Timer'
 
 function App() {
-  const name = "react";
 
   return (
-    <div className='app-header'>
-      <h1 className='test'>Hello, {
-        name === 'react' ? (<h1>YES</h1>) : null
-      }!!</h1>
-      <p>반갑습니다!</p>
+    <div className='container'>
+      <Todolist></Todolist>
+      <Clock></Clock>
     </div>
     
   )
